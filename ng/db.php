0d64e75568019a3a8f206c088e0d92e3063b4c14
chhengsamnang <samnang164@gmail.com>
2017-07-28 10:23:05 +0700

    remove all file

--- ng/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = new mysqli('localhost','root','123456','db_hotel');
-	if (mysqli_connect_errno()) 
-	{
-		die("failed to connect, the error message is:".mysqli_connect_error());
-	}
-
-?>
